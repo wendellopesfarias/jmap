a2021affd0c88a9c32bc6f7f6270423256f25f5c
wendelfariaslopes <wendellopesfarias@gmail.com>
2017-06-26 20:48:46 -0400

    More

---
+++ src/jmap/com/Gradient.java
@@ -0,0 +1,12 @@
+package jmap.com;
+
+public class Gradient {
+
+	public static final String[] COLOR = { "rgba(0, 255, 255, 0)", "rgba(0, 255, 255, 1)", "rgba(0, 191, 255, 1)",
+			"rgba(0, 127, 255, 1)", "rgba(0, 63, 255, 1)", "rgba(0, 0, 255, 1)", "rgba(0, 0, 223, 1)",
+			"rgba(0, 0, 191, 1)", "rgba(0, 0, 159, 1)", "rgba(0, 0, 127, 1)", "rgba(63, 0, 91, 1)",
+			"rgba(127, 0, 63, 1)", "rgba(191, 0, 31, 1)", "rgba(255, 0, 0, 1)" };
+	
+	public static final Double[] INTENSITY ={ .01,.1,.2,.3,.4,.5,.6,.7,.8,.9, 1.0};
+
+}
